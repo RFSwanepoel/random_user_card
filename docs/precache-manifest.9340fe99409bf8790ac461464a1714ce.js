0319ac0d5e3a9797345a2efe3501d32077166b41
Riaan <swanepoel.rf@gmail.com>
2019-05-21 23:31:49 +0200

    added docs folder for git pages.

---
+++ docs/precache-manifest.9340fe99409bf8790ac461464a1714ce.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6eb5f14e8ed3f2169bcee11b35c26b1",
+    "url": "/img/content-image.f6eb5f14.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6fb7ffd39143ae685857a4158457e24a",
+    "url": "/img/content-text.6fb7ffd3.svg"
+  },
+  {
+    "revision": "1f06a9e183714bd4ab77",
+    "url": "/css/chunk-vendors.6ad0b682.css"
+  },
+  {
+    "revision": "a614fcfae8c3a8f701371c5e63339de2",
+    "url": "/img/call.a614fcfa.svg"
+  },
+  {
+    "revision": "6584ecba8efeeaae417aeb493cef04fa",
+    "url": "/img/content-bullet.6584ecba.svg"
+  },
+  {
+    "revision": "15ff113db7ad6d5c870ea8c5672390c5",
+    "url": "/img/loading.15ff113d.svg"
+  },
+  {
+    "revision": "d641aab928fecfb2d441709d3da1fd7d",
+    "url": "/img/email.d641aab9.svg"
+  },
+  {
+    "revision": "56707eb1661745e6511837edb92d4200",
+    "url": "/img/user.56707eb1.svg"
+  },
+  {
+    "revision": "453a907bddab10950c0dea7344b9d022",
+    "url": "/img/locked.453a907b.svg"
+  },
+  {
+    "revision": "8c9806b4b67ea5f07dbb",
+    "url": "/js/app.d3fdfd6a.js"
+  },
+  {
+    "revision": "1f06a9e183714bd4ab77",
+    "url": "/js/chunk-vendors.172638e5.js"
+  },
+  {
+    "revision": "f29a501724759a3fd5533701358b8b75",
+    "url": "/img/home.f29a5017.svg"
+  },
+  {
+    "revision": "6b081cd4eb89cda43cf55810e5507f14",
+    "url": "/img/calendar.6b081cd4.svg"
+  },
+  {
+    "revision": "2696d9a91296ccb189c1c0156c3fddd9",
+    "url": "/img/cash.2696d9a9.svg"
+  },
+  {
+    "revision": "584a0fff9c1fde087fde0ec22e816e97",
+    "url": "/img/star.584a0fff.svg"
+  },
+  {
+    "revision": "d718ab9432b2da8d829ef486045e2822",
+    "url": "/img/location.d718ab94.svg"
+  },
+  {
+    "revision": "2f3634c4f6a783b8e84960d2fd22e9ee",
+    "url": "/img/content-section.2f3634c4.svg"
+  },
+  {
+    "revision": "a28cc8d1e3ae552bce170b1f04f5a518",
+    "url": "/img/hamburger.a28cc8d1.svg"
+  },
+  {
+    "revision": "f07bbf03ccdf1a9a36e86ca1c92abc90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c9806b4b67ea5f07dbb",
+    "url": "/css/app.51d1eef6.css"
+  }
+];
